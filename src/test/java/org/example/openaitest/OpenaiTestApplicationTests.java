44838ae2f1d9eafe72a83b1126bb2911587ec97e
coderyu5523 <coderyu5523@naver.com>
2024-06-28 17:12:03 +0900

    세팅 시작

---
+++ src/test/java/org/example/openaitest/OpenaiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.openaitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
